c484219fa072de8e0b91691c4e9297ede5b82967
James Chemobo <jkchemobo@gmail.com>
2022-08-24 03:06:19 +0300

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,15 @@
 require 'pry'
+require_relative './concerns/memorable'
+require_relative './concerns/paramable'
+require_relative './concerns/findable'
 
 class Artist
   attr_accessor :name
   attr_reader :songs
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include Paramable
 
   @@artists = []
 
